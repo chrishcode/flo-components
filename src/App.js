c1d1efe888a03558251bb71c5c394a868f00b3e9
chrisWohlfarth <chris.wohlfarth@gmail.com>
2018-11-14 07:10:57 +0100

    install tailwind and setup index page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <html>
+        <head>
+          <meta charset="utf-8" />
+          <title>Flo</title>
+          <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet" />
+          <link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css" rel="stylesheet" />
+        </head>
+        <body class="h-screen text-smooth">
+          <div className="App">
+            <div class="flex flex-col items-center justify-center h-full">
+                <a href="/" class="text-white hover:text-white no-underline">
+                    <div class="flex w-12 h-12 bg-brand rounded-full self-center items-center justify-center">
+                        <span class="font-logo text-xl leading-none tracking-tight">flo</span>
+                    </div>
+                </a>
+
+                <div class="bg-grey-lighter w-1/2 h-64 rounded-lg"></div>
+
+                <h1 class="bg-brand p-4 rounded-full text-4xl leading-none tracking-tight text-white mt-8 mb-8 rotate-2">Hi! I'm flo, a javascript scaffolding tool.</h1>
+                <p class="w-1/2 text-center">Flo is a javascript scaffolding tool. Scaffold starter apps and components to build user interfaces with amazing speed.</p>
+            </div>
+          </div>
+        </body>
+      </html>
     );
   }
 }
